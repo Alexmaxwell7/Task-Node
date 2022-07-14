75aa9e015d7f4e3945616f17d99c04b81931a3b1
Alex Maxwell <Alexmaxwell3096@gmail.com>
2022-07-14 17:35:27 +0530

    feat(added getcountry code)

--- index.js
+++ index.js
@@ -21,6 +21,34 @@ app.listen(5000,()=>{
     console.log("server Started")
 })
 
+//getcountry code
+
+const json_data=require('./country.json');
+
+let data=[json_data];
+
+let result_data=data.map((item)=>{
+    let constants=[{name:item.paramValues[1].name,description:item.paramValues[1].description,  value:item.paramValues[1].values,type:item.paramValues[1].type}];
+    let parameters=[];
+    let options=[
+    {
+        name:item.paramValues[0].name,
+        description:item.paramValues[0].description,
+        order:item.paramValues[0].rowNumber,
+        value:item.paramValues[0].values[0]
+    },
+    {
+        name:item.paramValues[0].name,
+        description:item.paramValues[0].description,
+        order:item.paramValues[0].rowNumber+1,
+        value:item.paramValues[0].values[1]
+    }
+    ];
+    let final_data={countryIsoAlpha2:item.countryIsoAlpha2,currencyCode:item.paramValues[0].defaultValue,constants,parameters,options}
+    return final_data;
+})
+console.log("final",result_data);
+
 
 module.exports = app;
 
